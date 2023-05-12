cd22340dcfa1aec1250e47557e0dcbeb1c3493ad
hayoenn2 <dwd1205@naver.com>
2023-05-13 01:26:34 +0900

    [Refac] 1844/hayeon

--- 2023.05-2week/hayeon/[JS] PRO.1844.게임맵최단거리 copy/게임맵의최단거리.js
+++ 2023.05-2week/hayeon/[JS] PRO.1844.게임맵최단거리 copy/게임맵의최단거리.js
@@ -1,4 +1,50 @@
 function solution(maps) {
+  const HEIGHT = maps.length;
+  const WIDTH = maps[0].length;
+  const moves = [
+    [-1, 0],
+    [1, 0],
+    [0, -1],
+    [0, 1],
+  ];
+  let result = -1;
+
+  const getIsVaild = (y, x) => {
+    if (x >= 0 && y >= 0 && x < WIDTH && y < HEIGHT && maps[y][x] === 1)
+      return true;
+  };
+
+  const bfs = (start, end) => {
+    const queue = [[start, end, 1]];
+
+    while (queue.length) {
+      const [curY, curX, curCount] = queue.shift();
+
+      if (curY === HEIGHT - 1 && curX === WIDTH - 1) {
+        result = curCount;
+        return;
+      }
+
+      moves.forEach((move) => {
+        const [dy, dx] = move;
+        const [ny, nx] = [curY + dy, curX + dx];
+
+        if (getIsVaild(ny, nx)) {
+          queue.push([ny, nx, curCount + 1]);
+          maps[ny][nx] = 0;
+        }
+      });
+    }
+  };
+
+  bfs(0, 0);
+
+  return result;
+}
+
+/*
+처음 풀이
+function solution(maps) {
   const queue = [];
   const n = maps.length;
   const m = maps[0].length;
@@ -30,3 +76,4 @@ function solution(maps) {
 
   return -1;
 }
+*/
