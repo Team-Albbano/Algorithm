84730836a10883e42d8b39d6c96f7b54136b3bcd
hayoenn2 <dwd1205@naver.com>
2023-06-10 18:39:53 +0900

    [JS]17686 수정

--- 2023.06-2week/hayeon/[JS] PRO.17686.파일명정렬/파일명정렬.js
+++ 2023.06-2week/hayeon/[JS] PRO.17686.파일명정렬/파일명정렬.js
@@ -1,14 +1,12 @@
 function solution(files) {
+  const HEAD_REGEX = /^\D+/;
+  const NUMBER_REGEX = /\d+/;
+
   return files.sort((a, b) => {
-    const aHead = a.match(/^\D+/)[0].toLowerCase();
-    const bHead = b.match(/^\D+/)[0].toLowerCase();
-
-    if (aHead < bHead) return -1;
-    if (aHead > bHead) return 1;
-
-    const aNum = a.match(/\d+/)[0].replace(/^0+/, "");
-    const bNum = b.match(/\d+/)[0].replace(/^0+/, "");
-
-    return aNum - bNum;
+    const aHead = a.match(HEAD_REGEX)[0].toLowerCase();
+    const bHead = b.match(HEAD_REGEX)[0].toLowerCase();
+    const aNum = a.match(NUMBER_REGEX)[0].replace(/^0+/, "");
+    const bNum = b.match(NUMBER_REGEX)[0].replace(/^0+/, "");
+    return aHead.localeCompare(bHead) || aNum - bNum;
   });
 }
