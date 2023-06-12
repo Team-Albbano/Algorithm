8a96dc4fdd33f5fe0a3e70be623bef483cda3fb0
hyunwlee <hwlee3928@gmail.com>
2023-06-12 10:01:45 +0900

    [JS]27866/hyunwlee

---
+++ 2023.06-2week/hyunwlee/[JS] BOJ.27866.문자와문자열/문자와 문자열.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution();
+  process.exit();
+});
+
+function solution() {
+  const [str, idx] = input;
+  console.log(str.charAt(idx - 1));
+}
