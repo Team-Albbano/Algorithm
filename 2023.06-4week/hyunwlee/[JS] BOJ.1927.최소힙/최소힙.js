137581a96f780d74a63aee8cc209235c5266d2b2
hyunwlee-dev <hwlee3928@gmail.com>
2023-06-21 12:22:26 +0900

    [JS]1927/hyunwlee

---
+++ 2023.06-4week/hyunwlee/[JS] BOJ.1927.최소힙/최소힙.js
@@ -0,0 +1,101 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on("line", function (line) {
+  input.push(line);
+}).on("close", function () {
+  solution();
+  process.exit();
+});
+
+function solution() {
+  const [n, ...arr] = input;
+  const pq = new PriorityQueue();
+  let ans = '';
+  arr.map(Number).forEach(item => {
+    if (item === 0) {
+      if (pq.isEmpty()) {
+        ans += 0 + '\n';
+      }
+      else {
+        ans += pq.poll() + '\n';
+      }
+    }
+    else {
+      pq.offer(item);
+    }
+  });
+  console.log(ans);
+}
+
+const top = 0;
+const parent = i => ((i + 1) >>> 1) - 1;
+const left = i => (i << 1) + 1;
+const right = i => (i + 1) << 1;
+
+class PriorityQueue {
+  constructor(comparator = (a, b) => a < b) {
+    this._heap = [];
+    this._comparator = comparator;
+  }
+  size() {
+    return this._heap.length;
+  }
+  isEmpty() {
+    return this.size() == 0;
+  }
+  peek() {
+    return this._heap[top];
+  }
+  offer(...values) {
+    values.forEach(value => {
+      this._heap.push(value);
+      this._siftUp();
+    });
+    return this.size();
+  }
+  poll() {
+    const polledValue = this.peek();
+    const bottom = this.size() - 1;
+    if (bottom > top) {
+      this._swap(top, bottom);
+    }
+    this._heap.pop();
+    this._siftDown();
+    return polledValue;
+  }
+  replace(value) {
+    const replacedValue = this.peek();
+    this._heap[top] = value;
+    this._siftDown();
+    return replacedValue;
+  }
+  _greater(i, j) {
+    return this._comparator(this._heap[i], this._heap[j]);
+  }
+  _swap(i, j) {
+    [this._heap[i], this._heap[j]] = [this._heap[j], this._heap[i]];
+  }
+  _siftUp() {
+    let node = this.size() - 1;
+    while (node > top && this._greater(node, parent(node))) {
+      this._swap(node, parent(node));
+      node = parent(node);
+    }
+  }
+  _siftDown() {
+    let node = top;
+    while (
+      (left(node) < this.size() && this._greater(left(node), node)) ||
+      (right(node) < this.size() && this._greater(right(node), node))
+    ) {
+      let maxChild = (right(node) < this.size() && this._greater(right(node), left(node))) ? right(node) : left(node);
+      this._swap(node, maxChild);
+      node = maxChild;
+    }
+  }
+}
