928eae0640d344885269c6573e9dc2e5cbef6b70
hyunwlee-dev <hwlee3928@gmail.com>
2023-07-29 22:46:42 +0900

    [JS]16928/hyunwlee (2)

--- 2023.07/2023.07-5week/hyunwlee/[JS] BOJ.16928.뱀과 사다리 게임/README.md
+++ 2023.07/2023.07-5week/hyunwlee/[JS] BOJ.16928.뱀과 사다리 게임/README.md
@@ -34,11 +34,18 @@ rl.on("line", function (line) {
   process.exit();
 });
 
+const mapping = (lines, map) => {
+  const [from, to] = lines.split(' ').map(Number);
+  map.set(from, to);
+}
+
 function solution() {
   const [nm, ...arr] = input;
   const [n, _] = nm.split(' ').map(Number);
-  const ladders = arr.splice(0, n).map(lines => lines.split(' ').map(Number));
-  const snakes = arr.map(lines => lines.split(' ').map(Number));
+  const ladders = new Map();
+  const snakes = new Map();
+  arr.splice(0, n).forEach(lines => mapping(lines, ladders));
+  arr.forEach(lines => mapping(lines, snakes));
   console.log(bfs(ladders, snakes));
 }
 
@@ -46,21 +53,32 @@ function bfs(ladders, snakes) {
   const queue = new LinkedList();
   queue.offer(1);
   const dist = Array.from({length: 101}, _ => 0);
-  const board = Array.from({length: 101}, (_, idx) => idx);
-  for (const [from, to] of [...ladders, ...snakes]) {
-    board[from] = to;
-  }
   while (!queue.isEmpty()) {
     const now = queue.poll(); 
     for (let i = 1; i <= 6; ++i) {
       let next = now + i;
       if (next > 100)
         continue;
-      next = board[next];
-      if (dist[next] !== 0)
-        continue;
-      dist[next] = dist[now] + 1;
-      queue.offer(next);
+      if (ladders.has(next)) {
+        next = ladders.get(next);
+        if (dist[next] !== 0)
+          continue;
+        dist[next] = dist[now] + 1;
+        queue.offer(next);
+      }
+      else if (snakes.has(next)) {
+        next = snakes.get(next);
+        if (dist[next] !== 0)
+          continue;
+        dist[next] = dist[now] + 1;
+        queue.offer(next);
+      }
+      else {
+        if (dist[next] === 0) {
+          dist[next] = dist[now] + 1;
+          queue.offer(next);
+        }
+      }
     }
   }
   return dist[100];
--- 2023.07/2023.07-5week/hyunwlee/[JS] BOJ.16928.뱀과 사다리 게임/뱀과 사다리 게임(내꺼).js
+++ 2023.07/2023.07-5week/hyunwlee/[JS] BOJ.16928.뱀과 사다리 게임/뱀과 사다리 게임(내꺼).js
@@ -12,11 +12,20 @@ rl.on("line", function (line) {
   process.exit();
 });
 
+const mapping = (lines, map) => {
+  const [from, to] = lines.split(' ').map(Number);
+  map.set(from, to);
+}
+
 function solution() {
   const [nm, ...arr] = input;
   const [n, _] = nm.split(' ').map(Number);
-  const ladders = arr.splice(0, n).map(lines => lines.split(' ').map(Number));
-  const snakes = arr.map(lines => lines.split(' ').map(Number));
+  const ladders = new Map();
+  const snakes = new Map();
+  arr.splice(0, n).forEach(lines => mapping(lines, ladders));
+  arr.forEach(lines => mapping(lines, snakes));
+  // console.log(ladders);
+  // console.log(snakes);
   console.log(bfs(ladders, snakes));
 }
 
@@ -24,27 +33,40 @@ function bfs(ladders, snakes) {
   const queue = new LinkedList();
   queue.offer(1);
   const dist = Array.from({length: 101}, _ => 0);
-  const route = Array.from({length: 101}, _ => '1 ');
-  const board = Array.from({length: 101}, (_, idx) => idx);
-  for (const [from, to] of [...ladders, ...snakes]) {
-    board[from] = to;
-  }
+  // const route = Array.from({length: 101}, _ => _);
+  // route[1] = '1';
   while (!queue.isEmpty()) {
     const now = queue.poll(); 
     for (let i = 1; i <= 6; ++i) {
       let next = now + i;
       if (next > 100)
         continue;
-      next = board[next];
-      if (dist[next] !== 0)
-        continue;
-      dist[next] = dist[now] + 1;
-      route[next] = route[now] + ` ${next}`;
-      queue.offer(next);
+      if (ladders.has(next)) {
+        next = ladders.get(next);
+        if (dist[next] !== 0)
+          continue;
+        dist[next] = dist[now] + 1;
+        // route[next] = route[now] + ` ${next}`;
+        queue.offer(next);
+      }
+      else if (snakes.has(next)) {
+        next = snakes.get(next);
+        if (dist[next] !== 0)
+          continue;
+        dist[next] = dist[now] + 1;
+        // route[next] = route[now] + ` ${next}`;
+        queue.offer(next);
+      }
+      else {
+        if (dist[next] === 0) {
+          dist[next] = dist[now] + 1;
+          // route[next] = route[now] + ` ${next}`;
+          queue.offer(next);
+        }
+      }
     }
   }
-  console.log(board)
-  console.log(route[100]);
+  // console.log(route[100]);
   return dist[100];
 }
 
@@ -85,3 +107,4 @@ class LinkedList {
     return this.size === 0;
   }
 }
+
